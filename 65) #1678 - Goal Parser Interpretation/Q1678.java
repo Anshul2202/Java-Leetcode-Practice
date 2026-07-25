d6a7229d28532dd157d50696e76699ec8cd28c2f
Anshul2202 <anshul220206@gmail.com>
2026-07-26 00:44:27 +0530

    Solved Question 1678

---
+++ 65) #1678 - Goal Parser Interpretation/Q1678.java
@@ -0,0 +1,10 @@
+class Solution1678 {
+    public String interpret(String command) {
+        
+        command = command.replaceAll("\\(\\)" , "o");
+        command = command.replaceAll("\\(al\\)" , "al");
+
+        return command;
+
+    }
+}
